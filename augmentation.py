f24c0234f85ba2badcf9dc8503379a3d06c94849
Jesse Phitidis <107813468+Jesse-Phitidis@users.noreply.github.com>
2023-11-23 20:37:10 +0000

    Update augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -11,6 +11,7 @@ class Rotation(tio.transforms.Transform):
 
     def __init__(self, p: float = 0.2):
         super().__init__(p=p)
+        self.T = tio.transforms.RandomAffine(scales=0, degrees=30, default_pad_value='otsu', image_interpolation='bspline', p=1)
 
     def apply_transform(self, subject):
 
@@ -20,9 +21,7 @@ class Rotation(tio.transforms.Transform):
             if isinstance(value, tio.ScalarImage) or isinstance(value, tio.LabelMap):
                 data_types[key] = value.data.dtype
 
-        T = tio.transforms.RandomAffine(scales=0, degrees=30, default_pad_value='otsu', image_interpolation='bspline', p=1)
-
-        subject = T(subject)
+        subject = self.T(subject)
 
         for key, value in subject.items():
             if isinstance(value, tio.ScalarImage) or isinstance(value, tio.LabelMap):
@@ -39,10 +38,9 @@ class Scaling(tio.transforms.Transform):
 
     def __init__(self, p: float = 0.2):
         super().__init__(p=p)
-
+        self.T = tio.transforms.RandomAffine(scales=(0.7, 1.4), degrees=0, isotropic=True, default_pad_value='otsu', image_interpolation='bspline', p=1)
+    
     def apply_transform(self, subject):
-
-        scale = random.uniform(0.7, 1.4)
 
         data_types = {}
 
@@ -50,9 +48,7 @@ class Scaling(tio.transforms.Transform):
             if isinstance(value, tio.ScalarImage) or isinstance(value, tio.LabelMap):
                 data_types[key] = value.data.dtype
 
-        T = tio.transforms.RandomAffine(scales=(scale, scale, scale, scale, scale, scale), degrees=0, default_pad_value='otsu', image_interpolation='bspline', p=1)
-
-        subject = T(subject)
+        subject = self.T(subject)
 
         for key, value in subject.items():
             if isinstance(value, tio.ScalarImage) or isinstance(value, tio.LabelMap):
@@ -65,24 +61,20 @@ class GaussianNoise(tio.transforms.Transform):
 
     def __init__(self, p: float = 0.15):
         super().__init__(p=p)
-
+        self.T = tio.transforms.RandomNoise(mean=0, std=(0, 0.316), p=1)
+    
     def apply_transform(self, subject):
-
-        T = tio.transforms.RandomNoise(mean=0, std=(0, 0.316), p=1)
-
-        return T(subject)
+        return self.T(subject)
     
 
 class GaussianBlur(tio.transforms.Transform):
     
         def __init__(self, p: float = 0.1):
             super().__init__(p=p)
+            self.T = tio.transforms.RandomBlur(std=(0.5, 1.5), p=1)
     
         def apply_transform(self, subject):
-    
-            T = tio.transforms.RandomBlur(std=(0.5, 1.5), p=1)
-    
-            return T(subject)
+            return self.T(subject)
 
 
 class Brightness(tio.transforms.Transform):
@@ -110,62 +102,41 @@ class Contrast(tio.transforms.Transform):
 
         x = random.uniform(0.65, 1.5)
 
-        min, max = self.get_min_max(subject)
+        min_max = self.get_min_max(subject)
 
         for key, value in subject.items():
             if isinstance(value, tio.ScalarImage):
                 scaled_data = value.data * x
-                clamped_data = torch.clamp(scaled_data, min, max)
+                clamped_data = torch.clamp(scaled_data, min_max[key][0], min_max[key][1])
                 value.set_data(clamped_data)
 
         return subject
     
     def get_min_max(self, subject):
 
-        min, max = 0, 0
-
+        min_max = {}
         for key, value in subject.items():
             if isinstance(value, tio.ScalarImage):
                 min = value.data.min()
                 max = value.data.max()
+                min_max[key] = (min, max)
         
-        return min, max
-    
+        return min_max
+
 
 class SimulateLowResolution(tio.transforms.Transform):
 
-    def __init__(self, p: float = 0.125):
+    def __init__(self, p: float = 0.25):
         super().__init__(p=p)
 
     def apply_transform(self, subject):
 
-        spacing = random.uniform(1, 2)
+        for image in subject.get_images():
+            T = tio.transforms.RandomAnisotropy(axes=(0,1,2), downsampling=(1,2), image_interpolation="bspline", p=0.5)
+            image.set_data(T(image.data))
 
-        input_spatial_shape = subject.spatial_shape
-        T_shape = tio.transforms.CropOrPad(input_spatial_shape)
+        return subject
 
-        T_down = tio.transforms.Resample(
-            target=spacing,
-            image_interpolation='nearest',
-            scalars_only=True,
-            p=1,
-        )
-
-        T_up = tio.transforms.Resample(
-            target=1,
-            image_interpolation='bspline',
-            scalars_only=True,
-            p=1
-        )
-
-        low_res_subject = T_up(T_down(subject))
-
-        for image in low_res_subject.get_images():
-            if image.spatial_shape != input_spatial_shape:
-                image.set_data(T_shape(image.data))
-
-        return low_res_subject
-    
 
 class Gamma(tio.transforms.Transform):
 
@@ -176,10 +147,11 @@ class Gamma(tio.transforms.Transform):
 
         gamma = random.uniform(0.7, 1.5)
 
-        min, max = self.get_min_max(subject)
+        min_max = self.get_min_max(subject)
 
         for key, value in subject.items():
             if isinstance(value, tio.ScalarImage):
+                min, max = min_max[key][0], min_max[key][1]
                 normalised_data = (value.data - min) / (max - min)
                 if random.random() < 0.15:
                     augmented_data = 1 - (1 - normalised_data) ** gamma
@@ -192,33 +164,31 @@ class Gamma(tio.transforms.Transform):
 
     def get_min_max(self, subject):
 
-        min, max = 0, 0
-
+        min_max = {}
         for key, value in subject.items():
             if isinstance(value, tio.ScalarImage):
                 min = value.data.min()
                 max = value.data.max()
+                min_max[key] = (min, max)
         
-        return min, max
+        return min_max
     
 
 class Mirroring(tio.transforms.Transform):
 
-    def __init__(self, p: float = 0.5):
+    def __init__(self, p: float = 1.0):
         super().__init__(p=p)
+        T_list = [tio.transforms.RandomFlip(axes=i, flip_probability=0.5) for i in range(3)]
+        self.T = tio.transforms.Compose(T_list)
 
     def apply_transform(self, subject):
-
-        T_list = [tio.transforms.RandomFlip(axes=i, flip_probability=1) for i in range(3) if random.random() < 0.5]
-
-        T_composed = tio.transforms.Compose(T_list)
-
-        return T_composed(subject)
+        return self.T(subject)
       
       
 class ComposednnUNetTransforms(tio.transforms.Transform):
 
-    def apply_transform(self, subject):
+    def __init__(self, p: float = 1.0):
+        super().__init__(p=p)
 
         T_list = [
             Rotation(),
@@ -232,7 +202,8 @@ class ComposednnUNetTransforms(tio.transforms.Transform):
             Gamma()
             ]
 
-        T_composed = tio.transforms.Compose(T_list)
+        self.T = tio.transforms.Compose(T_list)
 
-        return T_composed(subject)
+    def apply_transform(self, subject):
+        return self.T(subject)
         
